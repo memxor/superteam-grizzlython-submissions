27f547d56cfbe02876f77e07629c43d4af4ce0d9
Memxor <48633453+memxor@users.noreply.github.com>
2023-08-22 11:13:26 +0000

    Hyperdrive

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -9,12 +9,12 @@ const Header = () => {
         <div className="relative w-full">
           <div className="h-64 bg-gradient-to-t from-[#5522e0_-126.34%] md:h-128 lg:to-[rgba(0,0,0,0)_82.81%]">
             <img
-                className="h-64 w-full md:h-full"
-                src={`${router.basePath}/assets/images/site-image.png`}
-                alt="Superteam"
-              />
+              className="h-64 w-full md:h-full"
+              src={`${router.basePath}/assets/images/site-image.png`}
+              alt="Superteam"
+            />
             <div className="flex items-center justify-center">
-            <p className="z-10 m-0 text-center text-lg text-zinc-100 md:text-3xl absolute bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2">
+              <p className="absolute bottom-0 left-1/2 z-10 m-0 -translate-x-1/2 -translate-y-1/2 text-center text-lg text-zinc-100 md:text-3xl">
                 Explore a curated list of <br />
                 <span className="font-bold text-[#14F195]">
                   Hyperdrive
